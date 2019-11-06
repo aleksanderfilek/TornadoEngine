4d20a21953d8f6e5c648560b18ec75b39780382d
Aleksander <aleksanderfilek@wp.pl>
2019-11-06 16:20:50 +0100

    Mouse

--- TornadoEngine/inc/Mouse.h
+++ TornadoEngine/inc/Mouse.h
@@ -2,15 +2,18 @@
 #define MOUSE_H_
 
 #include<SDL2/SDL.h>
-
+#include"Texture.h"
 class Mouse{
-private:
-    SDL_Event *event;
-public:
-    Mouse();
-    ~Mouse();
-
-
+    public:
+        Mouse(const char* cursorPath, SDL_Renderer *renderer);
+        ~Mouse();
+        SDL_Point getMousePosition();
+        void render(SDL_Renderer *renderer);
+        bool getMouseDown(){return clicked;};
+        void update(const SDL_Event *e);
+    private:
+        Texture texture;
+        bool clicked = false;
 };
 
 #endif
