ab8fa04f6213085fd8769992b3a8c79b1e07e4ab
Aleksander <aleksanderfilek@wp.pl>
2019-11-09 21:48:06 +0100

    Texture finished

--- TornadoEngine/inc/Texture.h
+++ TornadoEngine/inc/Texture.h
@@ -9,7 +9,8 @@ private:
     SDL_Texture *texture;
     
     SDL_Rect sourceRect;
-    SDL_Rect destinationRect;
+    SDL_Point position;
+    SDL_Point scale = {1,1};
 public:
     //constructor
     Texture();
@@ -24,19 +25,23 @@ public:
 
     //rendering functions
     void render(SDL_Renderer *renderer);
-    void render(double angle, SDL_Point* center, SDL_RendererFlip flip = SDL_FLIP_NONE );
+    void render(SDL_Renderer *renderer,const double angle,const SDL_Point* center,const SDL_RendererFlip flip = SDL_FLIP_NONE );
 
-    void setPosition(int x, int y);
-    void setPosition(SDL_Point position);
-    void move(int x, int y);
-    void move(SDL_Point offset);
+    void setPosition(const int x, const int y) {position = {x,y};};
+    void setPosition(const SDL_Point _position){position = _position;};
+    void move(const int x, const int y){position = {position.x + x, position.y + y}; };
+    void move(const SDL_Point offset){position = {position.x + offset.x, position.y + offset.y};};
+
+    void setScale(const int x, const int y) { scale = {x,y};};
+    void setScale(const SDL_Point _scale){scale = _scale;};
 
     void setSourceRect(int x, int y, int w, int h){sourceRect={x,y,w,h};};
     void setSourceRect(SDL_Rect srcRect){sourceRect = srcRect;};
 
     //Properties
     SDL_Rect getSourceRect(){return sourceRect;};
-    SDL_Rect getDestinationRect(){return destinationRect;};
+    SDL_Rect getDestinationRect(){return {position.x, position.y,scale.x * sourceRect.w,scale.y * sourceRect.h};};
+    SDL_Point getScale(){return scale;};
 };
 
 #endif
--- TornadoEngine/src/Texture.cpp
+++ TornadoEngine/src/Texture.cpp
@@ -7,3 +7,50 @@ Texture::Texture(){
 Texture::~Texture(){
     
 }
+
+bool Texture::setTexture(const char *path, SDL_Renderer *renderer){
+    this->texture = loadTexture(path,renderer);
+        
+    return this->texture != NULL;
+}
+
+static SDL_Texture *loadTexture(const char *path, SDL_Renderer *renderer){
+    //The final texture
+    SDL_Texture* newTexture = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
+    }
+    else
+    {
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( renderer, loadedSurface );
+        if( newTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError() );
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    return newTexture;
+}
+
+void Texture::free(){
+    SDL_DestroyTexture( this->texture );
+    this->texture = NULL;
+    setSourceRect({0,0,0,0});
+}
+
+void Texture::render(SDL_Renderer *renderer){
+    SDL_RenderCopy(this->render,this->texture,&this->sourceRect,&getDestinationRect());
+}
+
+void Texture::render(SDL_Renderer *renderer,const double angle, const SDL_Point *center,const SDL_RendererFlip flip = SDL_FLIP_NONE){
+    SDL_RenderCopyEx(renderer, this->texture, &this->sourceRect , &getDestinationRect(),angle, center, flip);
+}
+
