ddeec042daa7bf0e828046c8c173c8f996b1d948
Aleksander Filek <filekaleksander@gmail.com>
2020-05-25 12:49:03 +0200

    builder

---
+++ Builder/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[]){
+	if(argc < 2){
+		printf("No instructions\n");
+		return 0;
+	}
+	
+	if(strcmp("builder", argv[1]) == 0){
+		if(strcmp("--release", argv[2]) == 0){
+
+		}
+		else{
+			
+		}
+	}
+	else {
+
+	}
+
+	return 0;
+}
